20ec4b1089bbefcefa8e2cbfdbb3043481783e86
angeltony392 <angeltony392@gmail.com>
2025-06-26 05:20:49 +0100

    Make Edit User Modal Scrollable

--- src/components/admin/UserManagement.tsx
+++ src/components/admin/UserManagement.tsx
@@ -367,7 +367,7 @@ export const UserManagement: React.FC = () => {
       {/* Edit User Modal */}
       {showEditModal && selectedUser && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
-          <div className="bg-white rounded-2xl max-w-md w-full p-6 relative animate-slide-up">
+          <div className="bg-white rounded-2xl max-w-md w-full p-6 relative animate-slide-up max-h-[90vh] overflow-y-auto">
             <button
               onClick={() => {
                 setShowEditModal(false);
